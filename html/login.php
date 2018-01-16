d73164c9a7f3270a251fc0702e6aa06d77b46c54
CodeGold <codegold79@gmail.com>
2018-01-15 19:53:53 -0800

    this time, really add the updated login.php page

--- html/login.php
+++ html/login.php
@@ -94,18 +94,18 @@ if(isset($_POST['btn-login']))
 		}
 		?>
 <br><br>
-				<p>new login: updated as of 1/15/2018. @sepuckett, do you know if "required" at the end of the <input> tag is all right? I added them but go ahead and delete them if you think these are deprecated, not useful, or perhaps there's a better way. ~CG</p>
+				<p>new login: updated as of 1/15/2018. @sepuckett, do you know if "required" at the end of the input tag is all right? I added them but go ahead and delete them if you think these are deprecated, not useful, or perhaps there's a better way. ~CG</p>
 				<form>
 				  <div class="form-group">
 				    <label for="exampleInputEmail1">Email address</label>
-				    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="txtemail" placeholder="Enter email" >
+				    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="txtemail" placeholder="Enter email" required>
 				    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 				  </div>
 				  <div class="form-group">
 				    <label for="exampleInputPassword1">Password</label>
 				    <input type="password" class="form-control" id="exampleInputPassword1" name="txtupass" placeholder="Password" required>
 				  </div>
-				  <button type="submit" class="btn btn-primary">Submit</button>
+				  <button type="submit" class="btn btn-primary" name="btn-login">Submit</button>
 				</form>
         </div>
         <br>
